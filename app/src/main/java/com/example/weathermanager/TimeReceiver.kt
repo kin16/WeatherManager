d66f659f2351471edd7fe1e71a00b48dec86f3ce
kin16 <nikbatishov114@gmail.com>
2020-01-31 13:18:31 +0500

    Upgrade notification, delete NotificationService

--- app/src/main/java/com/example/weathermanager/TimeReceiver.kt
+++ app/src/main/java/com/example/weathermanager/TimeReceiver.kt
@@ -12,22 +12,32 @@ import android.util.Log
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.preference.PreferenceManager
+import com.example.weathermanager.model.Model
+import com.example.weathermanager.model.WeatherDay
 import com.example.weathermanager.view.MainActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
 class TimeReceiver : BroadcastReceiver(){
+    private var temp: WeatherDay? = null
 
     override fun onReceive(context: Context?, intent: Intent?) {
         //Toast.makeText(context, "Alarm work", Toast.LENGTH_SHORT).show()
         Log.e("TimeReceiver", "Receive intent")
+
+        weather(context)
+    }
+
+    fun next(context: Context?){
         Log.e("TimeReceiver", "New Notification showing")
 
         val notificationManager =
-            context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // I would suggest that you use IMPORTANCE_DEFAULT instead of IMPORTANCE_HIGH
             val channel =
-                NotificationChannel("ID","NAME", NotificationManager.IMPORTANCE_HIGH)
+                NotificationChannel("ID", "NAME", NotificationManager.IMPORTANCE_HIGH)
             channel.enableVibration(true)
             channel.lightColor = Color.BLUE
             channel.enableLights(true)
@@ -36,9 +46,12 @@ class TimeReceiver : BroadcastReceiver(){
             notificationManager.createNotificationChannel(channel)
         }
 
+        Log.d("TimeReceiver", "working")
+
+        Log.d("TimeReceiver", "Temp = ${temp?.tempWithDegree}")
         val bigText = NotificationCompat.BigTextStyle()
-        bigText.bigText("Now the temperature is " + 9)
-        bigText.setBigContentTitle("Temperature has dropped")
+        bigText.bigText("Now the temperature is " + temp?.tempWithDegree)
+        bigText.setBigContentTitle("Temperature changed")
         bigText.setSummaryText("Temperature detail")
 
         val ii = Intent(context, MainActivity::class.java)
@@ -68,5 +81,19 @@ class TimeReceiver : BroadcastReceiver(){
         ed.putString("date", "")
         ed.putString("set", "")
         ed.commit()
+    }
+
+    fun weather(context: Context?) {
+        Model().getWeather()
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TimeReceiver", "Getting weather")
+                temp = it
+                next(context)
+            },{
+                Log.e("TimeReceiver", "Error")
+            }
+            )
     }
 }
